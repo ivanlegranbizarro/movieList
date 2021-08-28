43bd9a0770c09ff1ed4cc1e355e2bbd7e6d29cee
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2021-08-28 08:14:43 +0200

    Automatizar token user register

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -2,12 +2,27 @@ from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from user_app.api.serializers import RegistrationSerializer
+from rest_framework.authtoken.models import Token
+from user_app import models
 
 
 @api_view(['POST'])
 def registration_view(request):
     if request.method == 'POST':
         serializer = RegistrationSerializer(data=request.data)
+
+        data = {}
+
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializers.data)
+            account = serializer.save()
+
+            data['response'] = 'Registration successful!'
+            data['username'] = account.username
+            data['email'] = account.email
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+
+        else:
+            data = serializer.errors
+
+        return Response(data)
