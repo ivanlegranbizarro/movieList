55ae1c3e69b99a3965b66fecdbda8ee876795b84
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2021-08-30 14:30:08 +0200

    Retoques Pagination

--- watchlist_app/api/pagination.py
+++ watchlist_app/api/pagination.py
@@ -4,3 +4,6 @@ from rest_framework.pagination import PageNumberPagination
 class WatchListPagination(PageNumberPagination):
     page_size = 2
     page_query_param = 'p'
+    page_query_param = 'size'
+    max_page_size = 10
+    last_page_strings = 'end'
